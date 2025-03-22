9ba15e117f84b8eb32d3903f198562fcf87b237a
Galih956 <galihakbar886@gmail.com>
2025-03-22 10:32:27 +0700

    Pertemuan_2

---
+++ hello-react/src/App.js
@@ -0,0 +1,19 @@
+// import libraries
+import React from "react"
+import Header from "./header";
+// membuat komponen dengan class
+
+
+//create component
+const app = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>Nama saya galih </p>
+      <Header />
+    </div>
+  );
+};
+
+//export default
+export default app; 
---
+++ hello-react/src/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Makanan khas dari indonesia</h2>
+      </div> 
+    );
+  }
+};
+
+export default Header
